1fb107d56932074e3df8bf084f20274f6078b595
salmansheri <sheriffsalman00@gmail.com>
2023-07-12 00:13:51 +0530

    Generate parenthesis

---
+++ stack/generate-parenthesis/main.py
@@ -0,0 +1,27 @@
+class Solution:
+    def generate_parenthesis(self, n):
+        stack = []
+        res = []
+
+        def backtrack(openN, closeN):
+            if openN == closeN == n:
+                res.append("".join(stack))
+                return
+
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closeN)
+                stack.pop()
+
+            if closeN < openN:
+                stack.append(")")
+                backtrack(openN, closeN + 1)
+                stack.pop()
+
+        backtrack(0, 0)
+        return res
+
+
+n = 3 
+solution = Solution()
+print(solution.generate_parenthesis(n))
